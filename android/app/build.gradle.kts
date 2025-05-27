1a6c2a48817f99d50b17c17d64c481d814de5064
TheHiddenDeveloper <rodneyhagan74@gmail.com>
2025-05-27 18:49:56 +0000

    1. Swap from Groovy to Kotlin DSL

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,74 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.google.gms.google-services") 
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    FileInputStream(localPropertiesFile).use { localProperties.load(it) }
+}
+
+val flutterRoot = localProperties.getProperty("flutter.sdk")
+require(flutterRoot != null) {
+    "Flutter SDK not found. Define location with flutter.sdk in the local.properties file."
+}
+
+var flutterVersionCode = localProperties.getProperty("flutter.versionCode") ?: "1"
+var flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
+
+
+android {
+    namespace = "com.hypeteq.fitnessapp"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+    }
+
+    defaultConfig {
+        // TODO: Change this to your unique application ID.
+        // You can also set this in the local.properties file with flutter.applicationId.
+        applicationId = "com.hypeteq.fitnessapp"
+        minSdkVersion (23)
+        targetSdkVersion (33)
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        debug{
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+}
+
+flutter {
+    source("../..")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${rootProject.extra["kotlin_version"]}")
+}
